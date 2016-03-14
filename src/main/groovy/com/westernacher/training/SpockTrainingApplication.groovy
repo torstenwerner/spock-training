dc7660a92076f53c57f88a580a0d215a63992dca
Torsten Werner <torsten.werner@westernacher.com>
2016-03-14 20:23:25 +0100

    initial import

---
+++ src/main/groovy/com/westernacher/training/SpockTrainingApplication.groovy
@@ -0,0 +1,12 @@
+package com.westernacher.training
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SpockTrainingApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run SpockTrainingApplication, args
+	}
+}
---
+++ src/test/groovy/com/westernacher/training/SpockTrainingApplicationTests.groovy
@@ -0,0 +1,16 @@
+package com.westernacher.training
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.SpringApplicationConfiguration
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
+
+@RunWith(SpringJUnit4ClassRunner)
+@SpringApplicationConfiguration(classes = SpockTrainingApplication)
+class SpockTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
