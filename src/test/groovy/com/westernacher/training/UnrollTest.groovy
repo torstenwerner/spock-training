51b5444038764e5baa5fd41d622acb33a46a470f
Torsten Werner <torsten.werner@westernacher.com>
2016-04-23 12:20:00 +0200

    add UnrollTest

---
+++ src/test/groovy/com/westernacher/training/UnrollTest.groovy
@@ -0,0 +1,17 @@
+package com.westernacher.training
+
+import spock.lang.Specification
+import spock.lang.Unroll
+
+class UnrollTest extends Specification {
+    @Unroll
+    def "maximum of #a and #b is #c"() {
+        expect:
+        Math.max(a, b) == c
+
+        where:
+        a | b | c
+        1 | 2 | 1
+        4 | 5 | 4
+    }
+}
