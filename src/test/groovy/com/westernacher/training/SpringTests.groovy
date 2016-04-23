0186139f9245f3b31a0b176d68c95c487ae86f4d
Torsten Werner <torsten.werner@westernacher.com>
2016-04-23 12:11:00 +0200

    remove parts of less important documentation

--- src/test/groovy/com/westernacher/training/SpringTests.groovy
+++ src/test/groovy/com/westernacher/training/SpringTests.groovy
@@ -1,11 +1,7 @@
 package com.westernacher.training
 
-import org.junit.Test
-import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.SpringApplicationConfiguration
 import org.springframework.test.context.ContextConfiguration
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
 import spock.lang.Specification
 
 @ContextConfiguration(classes = SpockTrainingApplication)
@@ -13,9 +9,9 @@ class SpringTests extends Specification {
     @Autowired
     private String sampleBean;
 
-	void contextLoads() {
+    void contextLoads() {
         expect:
         sampleBean == 'Hello World!';
-	}
+    }
 
 }
